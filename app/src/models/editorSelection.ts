7761f3f83c6f37e126933064f73eb71c73059d78
Andreas Møller <andreas.moller@gmail.com>
2019-01-18 23:37:48 +0100

    update

--- app/src/models/editorSelection.ts
+++
@@ -1,4 +0,0 @@
-export type editorSelection =
-  | { type: "Element"; edit: boolean; elementId: string }
-  | { type: "Slide"; slideId: string }
-  | { type: "None" };
--- app/src/theme.ts
+++
@@ -1,4 +0,0 @@
-// export const primary = "#ECA106";
-export const success = "#8ACB88";
-export const warning = "#D34F1E";
-export const primary = "#88bc32";
