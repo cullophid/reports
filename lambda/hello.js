d5c00e400ed6a9692b89c56fe05e8fe0bc8c6de9
Andreas Møller <andreas.moller@gmail.com>
2019-01-19 19:08:05 +0100

    update

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,4 +1,6 @@
+console.log("Hello loaded");
 exports.handler = function(event, context, callback) {
+  console.log("Hello called");
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
