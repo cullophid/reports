cb2fa91d59e5a60a541907e1cfebee7427f58618
Andreas Møller <andreas.moller@gmail.com>
2019-01-22 01:19:46 +0100

    update

--- src/functions/import.js
+++
@@ -1,9 +0,0 @@
-console.log("hello called");
-const message = require("../message.js");
-exports.handler = function(event, context, callback) {
-  console.log("hello loaded");
-  callback(null, {
-    statusCode: 200,
-    body: message
-  });
-};
